f0598d731a651f432869b6c93cd8b78cc7902ce1
MexicanoGuy <77615747+MexicanoGuy@users.noreply.github.com>
2022-11-20 23:23:48 +0100

    Add files via upload

--- server/elephantSQL/conversation.sql
+++ server/elephantSQL/conversation.sql
@@ -2,5 +2,6 @@ CREATE TABLE conversation (
     conversationId SERIAL PRIMARY KEY,
     conversationTitle VARCHAR(50) NOT NULL,
     creator VARCHAR(30) NOT NULL,
-    participants TEXT[]
+    participants INTEGER[],
+    admins INTEGER[]
 );
