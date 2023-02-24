beec03790c860eb9f2037862e13a839d31f54612
MexicanoGuy <77615747+MexicanoGuy@users.noreply.github.com>
2023-02-24 22:55:00 +0100

    Add files via upload

--- server/socketEvents/create_new_chat.js
+++ server/socketEvents/create_new_chat.js
@@ -1,7 +1,6 @@
 module.exports = (io, socket, pool) =>{
   
     socket.on("create_new_chat", async (data) =>{
-      console.log(data);
       var users = [];
       users.push(data.creator);
       await pool.query("INSERT INTO conversation(conversationTitle, creator, participants, admins, pic) VALUES($1, $2, $3, $4, $5) ", [data.title, data.creator, users, users, data.pic]);
--- server/socketEvents/get_user_data.js
+++ server/socketEvents/get_user_data.js
@@ -2,18 +2,20 @@ module.exports = (io, socket, pool) =>{
     socket.on("get_user_data", async (data) =>{
         const findUserConv = await pool.query("SELECT DISTINCT * FROM conversation WHERE $1 = ANY(conversation.participants)",[data.userId]);
         const conversations = [];
-        for(let i=0; i < findUserConv.rowCount; i++){
-          let obj1 = {
-            convId: findUserConv.rows[i].conversationid,
-            title: findUserConv.rows[i].conversationtitle,
-            pic: findUserConv.rows[i].pic
+        if(findUserConv.rowCount > 0){
+          for(let i=0; i < findUserConv.rowCount; i++){
+            // console.log(findUserConv.rows[0])
+            let obj1 = {
+              convId: findUserConv.rows[i].conversationid,
+              title: findUserConv.rows[i].conversationtitle,
+              pic: findUserConv.rows[i].pic
+            }
+            conversations.push(obj1);
           }
-          conversations.push(obj1);
+          pool.end;
+          userToSocket(data.userId);
+          socket.emit("receive_user_data", conversations)
         }
-        pool.end;
-        userToSocket(data.userId);
-        socket.emit("receive_user_data", conversations)
-        
       });
     function userToSocket(userId){
       userId = parseInt(userId.trim(), 10);
