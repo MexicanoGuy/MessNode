93e528f382aaac512044894deb78e7d0771e50a1
MexicanoGuy <77615747+MexicanoGuy@users.noreply.github.com>
2023-04-17 23:31:51 +0200

    Delete server/mongoSchemas directory

--- server/mongoSchemas/ChatsSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-let ChatsSchema = new moongose.Schema({
-    chatName: { String, required: true},
-    userList: String
-});
-
-module.exports = mongoose.Schema('Chats', ChatsSchema);
